hoangduytran/blender-manual

commit 941b345927f666e1f9a4d9f49f5062604b7348aa

Restructure Scripts and Resources
The goal is to make the repository tree simpler and easier to understand.
Another goal is to make the structure similar between the Blender source repository and documentation.

- Rename `resource` to `build_files` -- this matches the Blender repo
- Move extensions to `build_files/extensions`
- Move make file utility scripts to `build_files/utils`-- akso matches Blender
- Create one `tools` directory with sub directories -- this also matches Blender
- Rename tools and move some around in the sub directories

Makefile
@@ -106,29 +106,29 @@ epubpdf: .SPHINXBUILD_EXISTS
 		--pdf-page-margin-bottom 50 \
 
 check_syntax:
-	@python3 tools_rst/rst_check_syntax.py --long --title --kbd > rst_check_syntax.log
+	@python3 tools/check_source/check_syntax.py --long --title --kbd > rst_check_syntax.log
 	@echo "Lines:" `cat rst_check_syntax.log | wc -l`
 	@python3 tools/open_quickfix_in_editor.py rst_check_syntax.log
 	@rm rst_check_syntax.log
 
 check_structure:
-	@python3 tools_rst/rst_check_images.py
+	@python3 tools/check_source/check_images.py
 
 check_spelling:
-	@python3 tools_rst/rst_check_spelling.py
+	@python3 tools/check_source/check_spelling.py
 
 checkout_locale:
-	@python3 ./tools_make/checkout_locale.py
+	@python3 ./build_files/utils/checkout_locale.py
 
 update_po:
-	@python3 ./tools_maintenance/update_po.py
+	@python3 ./tools/utils_maintenance/update_po.py
 
 report_po_progress:
-	@python3 tools_report/report_translation_progress.py --quiet \
+	@python3 tools/translations/report_translation_progress.py --quiet \
 	         `find locale/ -maxdepth 1 -mindepth 1 -type d -not -iwholename '*.git*' -printf 'locale/%f\n' | sort`
 
 update:
-	@python3 ./tools_make/make_update.py
+	@python3 ./build_files/utils/make_update.py
 
 format_py:
 	@autopep8 --in-place --recursive .

commit 2e3e18d089bb472fce6b66a01c8b0d7af40343e5

Fix incorrect paths in tools after relocating files
Regressions caused by 941b345927f666e1f9a4d9f49f5062604b7348aa.

Makefile
@@ -108,7 +108,7 @@ epubpdf: .SPHINXBUILD_EXISTS
 check_syntax:
 	@python3 tools/check_source/check_syntax.py --long --title --kbd > rst_check_syntax.log
 	@echo "Lines:" `cat rst_check_syntax.log | wc -l`
-	@python3 tools/open_quickfix_in_editor.py rst_check_syntax.log
+	@python3 tools/utils_ide/open_quickfix_in_editor.py rst_check_syntax.log
 	@rm rst_check_syntax.log
 
 check_structure:

tools/check_source/check_image_names.py
@@ -12,7 +12,7 @@ import re
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, ".."))
+ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", ".."))
 RST_DIR = os.path.join(ROOT_DIR, "manual", SUBDIR)
 LOCALE_DIR = os.path.join(ROOT_DIR, "locale")
 

tools/check_source/check_images.py
@@ -16,7 +16,7 @@ import re
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, ".."))
+ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", ".."))
 RST_DIR = os.path.join(ROOT_DIR, "manual", SUBDIR)
 LOCALE_DIR = os.path.join(ROOT_DIR, "locale")
 
@@ -73,8 +73,7 @@ def rst_files_report(img_refs):
     """
     Outputs the results of unused/missing images
     """
-    imgpath = os.path.normpath(os.path.join(os.path.abspath(
-        os.path.dirname(__file__)), "..", "manual", "images"))
+    imgpath = os.path.normpath(os.path.join(ROOT_DIR, "manual", "images"))
     img_files_set = set([f for f in os.listdir(imgpath)])
     img_refs_set = set(img_refs)
 

tools/check_source/check_internal_links.py
@@ -6,7 +6,7 @@ import io
 import argparse
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "manual"))
+ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "..", "manual"))
 EDIT_FILE = os.path.join(CURRENT_DIR, "broken_doc_links.txt")
 ROLE = ":doc:"
 

tools/check_source/check_syntax.py
@@ -9,7 +9,7 @@ import re
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "manual", SUBDIR))
+RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "..", "manual", SUBDIR))
 
 
 def rst_files(path):

tools/translations/po_shortcuts.py
@@ -12,7 +12,7 @@ import re
 import json
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-PO_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "locale"))
+PO_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "..", "locale"))
 
 ROLE = 'kbd'  # Change to any other role if needed
 

tools/utils_maintenance/rst_batch_edit.py
@@ -15,7 +15,7 @@ import os
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "manual", SUBDIR))
+RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "..", "manual", SUBDIR))
 
 
 def rst_files(path):

tools/utils_maintenance/rst_remap.py
@@ -65,7 +65,7 @@ def fatal(msg):
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "manual", SUBDIR))
+RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "..", "manual", SUBDIR))
 
 # name for temp file
 RST_MAP_ID = "rst_map.data"